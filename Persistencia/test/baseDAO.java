9004deb9f5a33ff4ae49d7714b465df97dc271e3
Joshiva Yzair <joshivatapia@gmail.com>
2022-10-28 14:48:47 -0700

    Dao creada
    Se creo una conexion a base de datos utilizando Mysql y JPA

---
+++ Persistencia/test/baseDAO.java
@@ -0,0 +1,22 @@
+
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
+
+public abstract class baseDAO<T> {
+    
+    protected EntityManager getEntityManager() throws PersistenceException{
+              EntityManagerFactory managerFactory =Persistence.createEntityManagerFactory("Kanban");
+              EntityManager entityManager = managerFactory.createEntityManager();
+              return entityManager;
+          }
+          
+          public abstract void agregar(T entidad);
+          public abstract void actualizar(T entidad);
+          public abstract void eliminar(long idEntidad);
+          public abstract T consultar(long idEntidad);
+          public abstract List<T> consultarTodo();
+}
